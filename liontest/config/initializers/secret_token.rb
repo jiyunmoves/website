e35a279a2fce42dce459cffaadcc7cc6ad71e969
jiyunmoves <jiyunmoves@gmail.com>
2014-02-25 09:21:30 +0900

    bashfiles

---
+++ liontest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liontest::Application.config.secret_key_base = 'ad5037cf80895cefe54b14a8c043c1a30c69113d0ba84c64122845eaa99f0e95b2fd135b61ef0e8ab17695d1804b25ca9c4609be8dc2adddac6872d8420ce72e'
